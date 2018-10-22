c04c556ddf72e541d80c7c1bb5137b138f314651
Lisa Lou <lisa.lou.aeh@gmail.com>
2018-10-22 23:13:20 +0200

    liaison des gossips avec leurs pages 'view'

--- gossip_project/lib/controller.rb
+++ gossip_project/lib/controller.rb
@@ -5,11 +5,20 @@ require 'gossips.rb'
 
 class ApplicationController < Sinatra::Base
     get '/' do
-      erb :index 
+      erb :index, locals: {gossips: Gossip.all, counter: Gossip.all.count}
     end
+
+    get '/gossips/:id' do
+      Gossip.all.each do |page|
+        page = params[:id]
+      end
+      erb :read_gossip, locals: {gossip: Gossip.all, page: params[:id]}
+    end
+
     get '/gossips/new/' do
       erb :new_gossips
     end
+
     post '/gossips/new/' do
       Gossip.new("#{params["gossip_author"]}", "#{params["gossip_content"]}").save
       redirect '/'
--- gossip_project/lib/gossips.rb
+++ gossip_project/lib/gossips.rb
@@ -1,5 +1,5 @@
 class Gossip
-    attr_accessor :author, :content
+    attr_accessor :author, :content, :id
     def initialize(author, content)
         @author = author
         @content = content
@@ -9,5 +9,14 @@ class Gossip
         CSV.open("./db/gossip.csv", "ab") do |csv|
             csv << [@author, @content]
         end
+    end
+
+    def self.all
+        all_gossips = []
+        #@id = 0 #compteur de gossip pour retrouver le n° du gossip
+        CSV.read("./db/gossip.csv").each do |csv_line|
+            all_gossips << Gossip.new(csv_line[0], csv_line[1])
+        end
+        return all_gossips
     end
 end
